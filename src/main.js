029c5dc2c225f1ad41c15c8445a3babc836e454f
Vitali Bestolkau <bestolkovv@gmail.com>
2021-12-04 21:21:41 +0100

    Overwrote firebase files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
  // import { BootstrapVue,  } from 'bootstrap-vue'
 
 
-// import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
 // import { getAuth } from 'firebase/auth'
 
 // import store
@@ -23,19 +23,19 @@ import router from './router'
 // Vue.config.productionTip = false
 
 createApp(App).use(router).use(store).mount('#app')
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCXIRdhpv9pw5kPujBJewPzkJm44iqjfSg",
-//   authDomain: "r6siege-guide.firebaseapp.com",
-//   projectId: "r6siege-guide",
-//   storageBucket: "r6siege-guide.appspot.com",
-//   messagingSenderId: "272522559720",
-//   appId: "1:272522559720:web:e3ed7658d8ad279a55895d",
-//   measurementId: "G-C5K9M28S5N"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCXIRdhpv9pw5kPujBJewPzkJm44iqjfSg",
+  authDomain: "r6siege-guide.firebaseapp.com",
+  projectId: "r6siege-guide",
+  storageBucket: "r6siege-guide.appspot.com",
+  messagingSenderId: "272522559720",
+  appId: "1:272522559720:web:e3ed7658d8ad279a55895d",
+  measurementId: "G-C5K9M28S5N"
+};
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-// initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 // const auth = getAuth()
 
