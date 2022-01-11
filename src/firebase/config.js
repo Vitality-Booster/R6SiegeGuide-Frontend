026ab9c38152d964be2e74f6d56a9eaa6abc0bb3
Vitali Bestolkau <bestolkovv@gmail.com>
2022-01-11 15:57:32 +0300

    Testing production

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,12 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
+console.log("process.env: " + process.env.VUE_APP_FIREBASE_API_KEY)
+console.log("process.env: " + process.env.VUE_APP_BASE_URL)
+console.log("process.env authDomain: " + process.env.VUE_APP_FIREBASE_AUTH_DOMAIN)
+console.log("process.env projectId: " + process.env.VUE_APP_FIREBASE_PROJECT_ID)
+console.log("string itself: AIzaSyDDmxXFFKyVQ_mV4sAx_DPCM38nhnCuqGE")
+
 const firebaseConfig = {
     apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
@@ -9,12 +15,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.VUE_APP_FIREBASE_APP_ID
 }
-
-// console.log("process.env: " + process.env.VUE_APP_FIREBASE_API_KEY)
-// console.log("process.env: " + process.env.VUE_APP_BASE_URL)
-// console.log("process.env authDomain: " + process.env.VUE_APP_FIREBASE_AUTH_DOMAIN)
-// console.log("process.env projectId: " + process.env.VUE_APP_FIREBASE_PROJECT_ID)
-// console.log("string itself: AIzaSyDDmxXFFKyVQ_mV4sAx_DPCM38nhnCuqGE")
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
