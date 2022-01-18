793629d62f893f70febb295350cf9119bbb429aa
Vitali Bestolkau <bestolkovv@gmail.com>
2022-01-18 12:18:59 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import store from './store/index'
 
 import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import router from './router'
 
 createApp(App).use(router).use(store).use(BootstrapVue3).mount('#app')
